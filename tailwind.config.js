cb19b0075f4b077d9069664ffc36c83a9aeeaef1
McNerdius <mcnerd@techie.com>
2021-06-30 01:27:12 -0600

    prune tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
     purge: [
         './Client/**/*.{razor,html}'
@@ -7,24 +5,11 @@ module.exports = {
     mode: 'jit',
     darkMode: 'media',
     theme: {
-        extend: {
-            colors: {
-                primary: colors.blue,
-                secondary: colors.amber,
-                accent: colors.lime
-            },
-            keyframes: {
-
-            },
-            animation: {
-            },
-            screens: {
-                'no-hover': { 'raw': '(hover:none)' }
-            },
+        extend:
+        {
             fontFamily: {
                 'sans': ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"]
             }
-        },
-    },
-    plugins: [],
+        }
+    }
 }
