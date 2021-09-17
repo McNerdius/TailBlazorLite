a0b7752c3d4afbd3433b37c3b0fe6d3b37b8802a
McNerdius <924403+McNerdius@users.noreply.github.com>
2021-09-17 06:10:38 -0600

    Rc1 (#13)
    * fixes move to .NET 6 RC1 #12
    
    * put deploy on latest RC1 build

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,22 +1,17 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace TailBlazorProject
-{
-    public class Program
-    {
-        public static async Task Main( string[] args )
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault( args );
-            builder.RootComponents.Add<App>( "#app" );
+using TailBlazorProject;
 
-            builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
+var builder = WebAssemblyHostBuilder.CreateDefault( args );
+builder.RootComponents.Add<App>( "#app" );
+builder.RootComponents.Add<HeadOutlet>( "head::after" );
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
+
+await builder.Build().RunAsync();
+
+
+builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
+
+await builder.Build().RunAsync();
