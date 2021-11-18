594fd1b70d6057971db6479c9c42ed0ea9e89f68
McNerdius <mcnerd@techie.com>
2021-11-18 08:32:19 -0700

    remove redundant RunAsync

--- Source/Program.cs
+++ Source/Program.cs
@@ -10,8 +10,3 @@ builder.RootComponents.Add<HeadOutlet>( "head::after" );
 builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
 
 await builder.Build().RunAsync();
-
-
-builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
-
-await builder.Build().RunAsync();
