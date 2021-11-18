7298569425a1f2a1fb4bd95c1dc633e17781d757
McNerdius <mcnerd@techie.com>
2021-11-18 08:32:07 -0700

    extract DarkSwitch

--- Source/Shared/MainLayout.cs
+++ Source/Shared/MainLayout.cs
@@ -6,32 +6,8 @@ namespace TailBlazorProject.Shared;
 
 public partial class MainLayout : LayoutComponentBase
 {
-
-    [Inject]
-    IJSRuntime JSRuntime
-    { get; set; }
-
-    private Task<IJSObjectReference> _module;
-    private Task<IJSObjectReference> Module => _module ??= JSRuntime.InvokeAsync<IJSObjectReference>( "import", "./Shared/MainLayout.razor.js" ).AsTask();
-
-    private string currentTheme = "light";
-    private bool isLightMode => currentTheme == "light";
-    private bool collapseNavMenu = true;
-
-    private async Task switchTheme()
-    {
-        var module = await Module;
-
-        currentTheme = currentTheme switch
-        {
-            "light" => "dark",
-            _ => "light"
-        };
-
-        await module.InvokeAsync<string>( "switchTheme", currentTheme );
-    }
-
     private string NavMenuCssClass => collapseNavMenu ? "hidden sm:block" : null;
+    private bool collapseNavMenu = true;
 
     private void ToggleNavMenu()
     {
